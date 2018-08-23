cd195de8fc237159b205c75a32a62557ef16e82e
ALEXANDR KAN <kan_off@mail.ru>
2018-08-24 04:19:32 +0600

    fifth homework

---
+++ 5th/project/js/script.js
@@ -0,0 +1,15 @@
+var btn 			 = document.getElementById('open-btn'),
+	inputValue		 = document.getElementsByClassName('goods-item').value,
+	buttons 		 = document.getElementsByTagName('button'),
+	chooseItems 	 = document.querySelector('div.main-functions input.choose-item'),
+	timeValue 		 = document.querySelector('div.main-functions input.time-value'),
+	countBudgetValue = document.querySelector('div.main-functions input.count-budget-value'),
+	employersItems 	 = document.querySelectorAll('div.main-functions input.hire-employers-item'),
+	name 			 = document.getElementsByClassName('name-value'),
+	budjet 			 = document.getElementsByClassName('budget-value'),
+	items 			 = document.getElementsByClassName('items-value'),
+	employers 		 = document.getElementsByClassName('employers-value'),
+	discount 		 = document.getElementsByClassName('discount-value'),
+	isopen 		     = document.getElementsByClassName('isopen-value');
+
+
